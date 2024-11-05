00ddfde257776b7b6e1debf4bceb1bee2cd7987e
AGuardiola20 <andres.pguardiola@gmail.com>
2024-11-05 06:53:34 -0600

    add: card components and types

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface Guest {
+  name: string;
+  age: number;
+  isMale: boolean;
+  plate: string;
+  idNumber: string;
+  checkInTime: string;
+  companions: number;
+  houseNumber: string;
+}
