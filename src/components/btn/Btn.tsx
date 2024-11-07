d5e4446537e294f5e8f0a8aae05518cb9b147756
isolanos <isolano506@gmail.com>
2024-11-07 01:48:01 -0600

    Merge branch 'main' of https://github.com/AGuardiola20/safegate-project-react into add-btn

--- src/components/btn/Btn.tsx
+++ src/components/btn/Btn.tsx
@@ -1,6 +1,16 @@
-const Btn = () => {
-  return;
-  <div>
-    <h1>Boton</h1>
-  </div>;
+import "./Btn.css";
+
+type ButtonProps = {
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+export const Btn = (props: ButtonProps) => {
+  return (
+    <div>
+      <h1>Componente</h1>
+      <button onClick={props.handleClick} className="boton">presionar</button>
+    </div>
+  );
 };
+
+export default Btn;
