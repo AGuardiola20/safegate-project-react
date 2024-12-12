89ec403bcdb673e3f2aed1b163def17cc220cac7
AGuardiola20 <andres.pguardiola@gmail.com>
2024-12-11 23:19:49 -0600

    add: credentials

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,14 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyDStQYcuDBfK31zeZYLzIukVqm8g9RgBtw",
+  authDomain: "safegate-20d87.firebaseapp.com",
+  projectId: "safegate-20d87",
+  storageBucket: "safegate-20d87.firebasestorage.app",
+  messagingSenderId: "587967659850",
+  appId: "1:587967659850:web:b321a602cf6b9983100bb3",
 };
-
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
